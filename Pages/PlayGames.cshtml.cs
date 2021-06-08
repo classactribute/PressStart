3f070e6de2743ff040e2c08f0f4fd665a6577fb7
gataki <nxenos@me.com>
2021-06-08 14:50:03 -0400

    emulator engine, play screen db prep

--- Pages/PlayGames.cshtml.cs
+++ Pages/PlayGames.cshtml.cs
@@ -4,11 +4,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PressStart.Data;
+using PressStart.Models;
 
 namespace PressStart.Pages
 {
     public class PlayGamesModel : PageModel
     {
+
+        private readonly PressStartContext db;
+        
+        public PlayGamesModel(PressStartContext db) => this.db = db;
+
+        [BindProperty(SupportsGet = true)]
+
+        public int Id{get; set;}
+
+        public Models.Game game {get;set;}
+
+        public Models.Comment comment {get;set;}
+
+        public async Task onGetAsync() => game = await db.Games.FindAsync(Id);
+
+        public List<Comment> comments{get;set;}= new List<Comment>();
+
+        //public async Task onGetAsync() => comments = await db.Comments.FirstOrDefault(x => x.GameId == Id);
         public void OnGet()
         {
         }
