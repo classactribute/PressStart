0612c098c5768e73fbeafa2a0cf0c9f23389072f
Tee <51485909+classactribute@users.noreply.github.com>
2021-06-10 19:06:35 -0400

    Admin Index
    Trying to get the table to work

--- Pages/Admin/Index.cshtml.cs
+++ Pages/Admin/Index.cshtml.cs
@@ -6,14 +6,30 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 using Microsoft.AspNetCore.Authorization;
+using PressStart.Data;
+using PressStart.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace PressStart.Pages
 {
     [Authorize(Roles = "Admin")]
     public class AdminIndexModel : PageModel
     {
-        public void OnGet()
+        //public AdminIndexModel(PressStartContext db) => this.db = db;
+        public List<User> UserList { get; set; } = new List<User>();
+
+        private readonly PressStartContext db;
+        private readonly ILogger<AdminIndexModel> _logger;
+        public AdminIndexModel(PressStartContext db, ILogger<AdminIndexModel> logger)
+                {
+                    this.db = db;
+                    _logger = logger;
+                }
+
+        public async Task OnGetAsync()
         {
+            UserList = await db.Users.ToListAsync();
         }
     }
 }
