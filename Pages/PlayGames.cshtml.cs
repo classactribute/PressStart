72259c0fcf1274d4f4f6bdad8a58b4cbcc21e970
Tee <51485909+classactribute@users.noreply.github.com>
2021-06-10 13:23:09 -0400

    Featured Game / Game Play
    GAMES ARE PLAYABLE NOW IN PLAYGAMES!

--- Pages/PlayGames.cshtml.cs
+++ Pages/PlayGames.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using PressStart.Data;
 using PressStart.Models;
 
@@ -17,20 +18,23 @@ namespace PressStart.Pages
         public PlayGamesModel(PressStartContext db) => this.db = db;
 
         [BindProperty(SupportsGet = true)]
+        
 
         public int Id{get; set;}
+        public async Task OnGetAsync() => Game = await db.Games.FindAsync(Id);
 
-        public Models.Game game {get;set;}
+        public Game Game {get;set;}
 
-        public Models.Comment comment {get;set;}
+        public Comment comment {get;set;}
 
-        public async Task onGetAsync() => game = await db.Games.FindAsync(Id);
+        public async Task onGetAsync(int? id)
+        {
+            Game = await db.Games.FirstOrDefaultAsync(m => m.GameId == id);
+        }
 
+        
         public List<Comment> comments{get;set;}= new List<Comment>();
 
         //public async Task onGetAsync() => comments = await db.Comments.FirstOrDefault(x => x.GameId == Id);
-        public void OnGet()
-        {
-        }
     }
 }
