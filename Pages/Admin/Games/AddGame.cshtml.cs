c510377b9317e45895ea57d1dc00d794efc84b61
gataki <nxenos@me.com>
2021-06-10 12:20:10 -0400

    :(

--- Pages/Admin/Games/AddGame.cshtml.cs
+++ Pages/Admin/Games/AddGame.cshtml.cs
@@ -10,14 +10,29 @@ using Microsoft.Extensions.Logging;
 using System.ComponentModel.DataAnnotations;
 using PressStart.Data;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using PressStart.Data.Controllers;
+using Amazon.S3.Model;
+using Amazon;
+using Amazon.Runtime;
+using Amazon.S3;
+
+
 namespace PressStart.Pages.Admin.Games
 {
+
+
+  
 
     [Authorize(Roles = "Admin")]
     public class AddGameModel : PageModel
     {
         private PressStartContext db;
     private readonly ILogger<RegisterModel> logger;
+
+    
+    private IHostingEnvironment _environment;
+
     public AddGameModel(PressStartContext db, ILogger<RegisterModel> logger) {
         this.db = db;
         this.logger = logger;
@@ -37,20 +52,75 @@ namespace PressStart.Pages.Admin.Games
 
     [BindProperty]
     public string Description { get; set; }
+
+    [BindProperty]
     
-    
-    public async Task<IActionResult> OnPostAsync()
+    public IFormFile UploadThumb {get;set;}
+
+    [BindProperty]
+
+    public IFormFile UploadRom { get; set; }
+
+
+
+
+        public async Task<IActionResult> OnPostAsync()
     {
       if (ModelState.IsValid)
       {
         //var userName = User.Identity.Name; // userName is email
         //var user = db.Users.Where(u => u.UserID == userName).FirstOrDefault(); // find user record
+        string ThumbnailPath = null;
+        
+        if(UploadThumb != null){
+        
+        //optional
+        string fileExtension = Path.GetExtension(UploadThumb.FileName).ToLower();
+        
+        
+        var file = Path.Combine(_environment.ContentRootPath,"wwwroot","Uploads", UploadThumb.FileName );
+        
+        
+        using(var fileStream = new FileStream(file, FileMode.Create))
+        {
+          await UploadThumb.CopyToAsync(fileStream);
+          
+        }
+        
+        ThumbnailPath = Path.Combine("Uploads", UploadThumb.FileName);
+        }
+        
+        if(UploadRom != null){
+            var bucketName = "presssroms";
+            var AWSKey = keys.AWSKey;
+            var AWSSecret = keys.AWSSecret;
+            var AWSRegion = RegionEndpoint.GetBySystemName("us-east-1");
+            var cred = new BasicAWSCredentials(AWSKey, AWSSecret);
+            var client = new AmazonS3Client(cred, AWSRegion);
+            var filename = UploadRom.FileName;
+            PutObjectRequest request = new PutObjectRequest()
+                {
+                 InputStream = UploadRom.OpenReadStream(),
+                 BucketName = "presssroms",
+                 Key = filename
+                };
+            PutObjectResponse response = await client.PutObjectAsync(request);
+            GamePath = Path.Combine("https://presssroms.s3.amazonaws.com/", UploadRom.FileName);
+
+                    
+          }
+
+                
+        
         var newGame = new PressStart.Models.Game { GameName = GameName, GameType = GameType, GamePath = GamePath , ThumbnailPath = ThumbnailPath, Description = Description };
+        
         db.Add(newGame);
         await db.SaveChangesAsync();
         return RedirectToPage("AddGameSuccess");
       }
-      return Page();
+      return Page();  
+      
+      
     }
   }
 }
